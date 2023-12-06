795d42d54abc43525ec72603020543e35cd864ea
TinKosmina <tin.kosmina@gmail.com>
2023-12-06 18:44:15 +0100

    new

---
+++ chat-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.scss";
+import LoginPage from "./Components/LoginPage";
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
