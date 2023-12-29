1ac7582f661b10c983bfd383f3be7e53a04866a2
TinKosmina <tin.kosmina@gmail.com>
2023-12-29 14:39:04 +0100

    sa

--- chat-app/src/Components/MainPage.jsx
+++ chat-app/src/Components/MainPage.jsx
@@ -1,11 +1,37 @@
 import Navigation from "./Navigation";
+import { useState } from "react";
 
 export default function MainPage(){
+
+    const [messages, setMessage] = useState([]);
+    const [messageInput, setMessageInput] = useState();
+
+    function handleSubmit(e){
+        e.preventDefault();
+        setMessage([...messages, messageInput])
+        setMessageInput('');
+    }
+
+
     return(
         <div>
             <Navigation></Navigation>
         <h1 class="mainPageHeader">Welcome, This is the main page!</h1>
+        <div className="messagesSection">
+            <form onSubmit={handleSubmit}>
+                <label>Send message:
+                <input type="text" value={messageInput} onChange={(e) => setMessageInput(e.target.value)}/>
+                </label>
+                <button type="submit">Send</button>
+            </form>
+            <ul>
+                {messages.map((message,index)=>{
 
+                        return <li key={index}>{message}</li>
+
+                })}
+            </ul>
+        </div>
         </div>
     )
 }
